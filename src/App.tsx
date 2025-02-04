9cfb2de4c9b08647c8cc27304766494599e4d447
Sina Saadat <escm94.sinasaadat@gmail.com>
2025-02-04 21:16:02 +0330

    config tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div>React Framework</div>;
+  return (
+    <div className="w-full h-screen flex items-center justify-center text-lg">
+      React Framework
+    </div>
+  );
 }
 
 export default App;
