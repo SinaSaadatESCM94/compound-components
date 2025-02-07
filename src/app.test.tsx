567bddf5181491a3726e25fa8b034a9d57a9a222
Sina Saadat <escm94.sinasaadat@gmail.com>
2025-02-07 09:33:57 +0330

    config testing library

---
+++ src/app.test.tsx
@@ -0,0 +1,15 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import App from "./App";
+
+describe("App Component", () => {
+  it('renders the text "React Framework"', () => {
+    render(<App />);
+    const element = screen.getByText("React Framework");
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveClass(
+      "w-full h-screen flex items-center justify-center text-lg"
+    );
+  });
+});
