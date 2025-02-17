eac3e7b823fe05e6341acba2c2ee4cf2b8772d5e
Sina Saadat <escm94.sinasaadat@gmail.com>
2025-02-17 23:12:06 +0330

    init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,68 @@
+import { ChangeEvent, useState } from "react";
+import {
+  NonCompoundInput,
+  NonCompoundInputProps,
+  CompoundInput,
+  CompoundInputProps,
+  Fieldset,
+  Label,
+  ErrorMessage,
+} from "./components";
 import "./App.css";
 
 function App() {
+  //#region states
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [errorFirstName, setErrorFirstName] = useState<string>("");
+  const [errorLastName, setErrorLastName] = useState<string>("");
+  //#endregion
+
+  //#region actions
+  const onChangeFirstName = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setFirstName(value);
+    setErrorFirstName(!value ? "field required" : "");
+  };
+  const onChangeLastName = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setLastName(value);
+    setErrorLastName(!value ? "field required" : "");
+  };
+  //#endregion
+
+  //#region DOM
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-light">
-      React Framework
-    </div>
+    <main className="w-full h-screen flex flex-col items-center justify-center bg-light app-container p-6 gap-6">
+      <Fieldset className="w-full md:w-1/2 xl:w-1/3">
+        <NonCompoundInput
+          tabIndex={1}
+          id="firstName"
+          name="firstName"
+          type="text"
+          label="First Name"
+          placeholder="John"
+          value={firstName}
+          errorMessage={errorFirstName}
+          onChange={onChangeFirstName}
+        />
+      </Fieldset>
+      <Fieldset>
+        <Label text="Last Name" />
+        <CompoundInput
+          tabIndex={2}
+          id="lastName"
+          name="lastName"
+          type="text"
+          placeholder="Doe"
+          value={lastName}
+          onChange={onChangeLastName}
+        />
+        <ErrorMessage textError={errorLastName} />
+      </Fieldset>
+    </main>
   );
+  //#endregion
 }
 
 export default App;
--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,12 +1,12 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import App from "./App";
 
 describe("App Component", () => {
   it('renders the text "React Framework"', () => {
     render(<App />);
-    const element = screen.getByText("React Framework");
+    const element = document.querySelector(".app-container");
     expect(element).toBeInTheDocument();
     expect(element).toHaveClass(
       "w-full h-screen flex items-center justify-center bg-light"
